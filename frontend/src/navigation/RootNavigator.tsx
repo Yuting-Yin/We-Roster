baf56fd7f9462bbeda8bc2ef00284c7d4b0bc4f9
Yuting-Yin <yuting.yin@student.unimelb.edu.au>
2025-09-08 22:55:07 +1000

    fix navigator

--- frontend/src/navigation/RootNavigator.tsx
+++ frontend/src/navigation/RootNavigator.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from '@/screens/Splash';
-import Login from '@/screens/Login/Login';
+import Login from '@/screens/Login';
 
 export type RootStackParamList = { Splash: undefined; Login: undefined; };
 const Stack = createNativeStackNavigator<RootStackParamList>();
